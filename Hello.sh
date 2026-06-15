42bd87630c8be2a0c1bbe3f91ff7b2f645a993a7
Marie Mooney <99214787+drswoop@users.noreply.github.com>
2026-06-15 20:44:28 +0000

    I got the echo command working!

--- Hello.sh
+++ Hello.sh
@@ -5,4 +5,6 @@
 #Please modify the file to print "Hello World, my name is __" to the terminal, replacing the blank with your name. 
 #Then save the file and run it in the terminal to see the result.'
 
-#Code goes here: 
+#Code goes here
+
+echo "Hello World, my name is myName!" 
