e28ad83345cc30258da7aa397b59a3073b6f4789
Marie Mooney <99214787+drswoop@users.noreply.github.com>
2026-06-15 19:08:25 +0000

    reorganize file structure and add comments to Hello.sh

---
+++ Hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#This is a comment. The computer will ignore it - it is for humans. 
+
+<<'This is also a comment. 
+But you can let it go on for more than one line 
+when you start it like above and enclose the comments in apostrophies. 
+Please modify the file to print "Hello World, my name is __" to the terminal, replacing the blank with your name. 
+Then save the file and run it in the terminal to see the result.'
+
+#Code goes here: 
