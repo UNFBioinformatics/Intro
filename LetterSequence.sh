492bba0167f105726819d45310cb643994cb0a61
kylasanders1223-wq <kylasanders1223@gmail.com>
2026-06-23 20:23:43 -0400

    Update LetterSequence.sh

--- LetterSequence.sh
+++ LetterSequence.sh
@@ -6,3 +6,5 @@
 #and to use the comments to show that you can explain your code.
 echo "You've entered the DNA sequence game."
 echo "Your goal: to memorize a DNA nucleotide sequence given to you one letter at a time (A, T, C,G) until you get five of the same letter in a row."
+echo "If you enter the sequence incorrectly, you will be taken back to the beginning."
+
