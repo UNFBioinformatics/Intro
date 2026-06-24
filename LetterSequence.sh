1977e74a6073d721e4ebf19493366c197f303a34
kylasanders1223-wq <kylasanders1223@gmail.com>
2026-06-23 20:11:39 -0400

    Update LetterSequence.sh

--- LetterSequence.sh
+++ LetterSequence.sh
@@ -4,3 +4,6 @@
 
 #Make sure to record your thoughts and instructions in your OWN words in the comments 
 #and to use the comments to show that you can explain your code.
+
+echo "You've entered the DNA sequence game"
+echo "The goal: create and remember a sequence of nucleotides until you get 5 in a row of the same letter."
