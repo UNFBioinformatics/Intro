c2a22c8740be44ed387ebae364938f641bc34477
UNF-Hashim <N01580861@unf.edu>
2026-07-07 16:53:26 +0000

    Generate a letter sequence for round 1 and allow the user to type it in

--- LetterSequence.sh
+++ LetterSequence.sh
@@ -15,3 +15,37 @@ sleep 2
 echo "Try your best!"
 sleep 3
 clear
+
+while [ $round -le 3 ]
+do
+    echo "Round $round"
+    echo
+
+    # decide how long the sequence is
+    if [ $round -eq 1 ]
+    then
+        length=3
+    elif [ $round -eq 2 ]
+    then
+        length=5
+    else
+        length=10
+    fi
+
+
+    sequence=""
+
+    i=0
+    while [ $i -lt $length ]
+    do
+        random=${letters[$RANDOM % 4]}
+        sequence="$sequence$random"
+        i=$((i+1))
+    done
+
+    echo "$sequence"
+    sleep 3
+    clear
+
+    echo "type the sequence:"
+    read answer
