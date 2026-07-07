f781b8a99704a4c5b13a5f4ae7767f4f13ac9280
UNF-Hashim <N01580861@unf.edu>
2026-07-07 16:45:14 +0000

    Start of the game

--- LetterSequence.sh
+++ LetterSequence.sh
@@ -4,3 +4,14 @@
 
 #Make sure to record your thoughts and instructions in your OWN words in the comments 
 #and to use the comments to show that you can explain your code.
+
+letters=("A" "T" "G" "C")
+round=1
+
+echo "Welcome to Letter Sequence!"
+sleep 2
+echo "Each round is a new random sequence"
+sleep 2
+echo "Try your best!"
+sleep 3
+clear
