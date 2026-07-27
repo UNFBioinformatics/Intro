4fcdfb56b74e19b5712054a2aaa1a42a0e574f13
AbsentYolo <n01539287@unf.edu>
2026-07-27 02:35:22 +0000

    Update Penguin_Tamer analysis and rendered HTML

---
+++ .quarto/vdoc/.vdoc.52ec5d6d-e11d-4d3e-aa83-4b93ae3b589e.r
@@ -0,0 +1,35 @@
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+install.packages("rmarkdown") 
+
+#The pound sign controls titles outside of the code sections; it allows you to make comments when you are *INSIDE* code spaces 
+
+#Our first piece of code. Lets say hello: 
+print("Hello, World!")
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#We started another piece of code! Look how we can go back and forth between writing explainations and writing code! 
+summary(rnorm(100)) 
+#
+#
+#
+#
