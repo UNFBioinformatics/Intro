f804d7b7b68b1d5254068459928ed54ad6ffaebe
OliviaHagedoorn <oliviajhag@gmail.com>
2026-09-25 18:43:57 +0000

    Ib While Interrupted Finished Assignment

--- samples/WhileInterrupted.sh
+++ samples/WhileInterrupted.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
-
+# begin line counter 
 i=1
-
-while read -r line  
-
-do  
-    if [ $i -lt 10 ]
-    then
-        echo $line
-        sleep 1
-        ((i++))
-    else 
-        break 
-    fi
+# read each line from input file
+while read -r line
+# start loop
+do
+# check current line number is less or equal to requested number
+if [ $i -le $1 ]
+# begin conditional statement
+then
+# print current line
+echo "$line"
+# pause before printing next line
+sleep 1
+# increase line counter by 1
+((i++))
+# if request lines reached then stop
+else
+# stop the loop
+break
+# end conditional statement
+fi
+# end the loop
 done
-
-#More instructions after the loop 
+# tell who made script 
+echo "This script was made by Olivia Hagedoorn."
--- samples/infiniteNoMore.sh
+++ samples/infiniteNoMore.sh
@@ -1,38 +1,27 @@
 #!/bin/bash
-
-# Initialize the loop counter.
+# start loop
 i=0
-
-# Initialize the hand variable.
+# initialize hand variable
 hand=0
-
-# Continue looping until hand equals 6.
+# continue looping until hand is 6
 while [ $hand -ne 6 ]
-
-# Start the loop.
+# start loop
 do
-    # Generate a random number between 1 and 10.
-    hand=$(( (RANDOM % 10) + 1 ))
-
-    # Print the generated number.
-    echo $hand
-
-    # Increase the loop counter by one.
-    ((i++))
-
-    # Check whether the loop has run 10 times.
-    if [ $i -eq 10 ]
-
-    # Begin the conditional statement.
-    then
-        # Force the exit condition to 6.
-        hand=6
-
-    # End the conditional statement.
-    fi
-
-    # Reduce the delay to make the loop run faster.
-    sleep 0.1
-
-# End the loop.
+# generate random number 1-6
+hand=$(( (RANDOM % 6) + 1 ))
+# print generated number
+echo $hand
+# increase counter by one
+((i++))
+# check loop run 10 times
+if [ $i -eq 10 ]
+# begin conditional statement
+then
+# force exit condition to be 6
+hand=6
+# end conditional statement
+fi
+# reduce delay to make loop faster
+sleep 0.1
+# end the loop
 done
