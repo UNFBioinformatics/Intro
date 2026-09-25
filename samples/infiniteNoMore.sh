e360a0432ac086128d51a6f0695359d4a81dc5ca
OliviaHagedoorn <oliviajhag@gmail.com>
2026-09-25 18:52:25 +0000

    Ia Infinite No More Finished Assignment

--- samples/infiniteNoMore.sh
+++ samples/infiniteNoMore.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-# start loop
+# start loop counter
 i=0
-# initialize hand variable
+# start hand variable 
 hand=0
 # continue looping until hand is 6
 while [ $hand -ne 6 ]
 # start loop
 do
 # generate random number 1-6
-hand=$(( (RANDOM % 6) + 1 ))
+hand=$(( (RANDOM % 6 ) + 1 ))
 # print generated number
 echo $hand
-# increase counter by one
+# increase loop counter by one
 ((i++))
-# check loop run 10 times
+# check loop ran 10 times
 if [ $i -eq 10 ]
 # begin conditional statement
 then
-# force exit condition to be 6
+# force exit condition 6
 hand=6
 # end conditional statement
 fi
 # reduce delay to make loop faster
 sleep 0.1
-# end the loop
+# end loop
 done
